39b4ebd13e37e7b033cd7af5fc64693f843dacd6
Maciej Janowski <maciej@janowski.co>
2018-01-15 15:13:49 +0000

    first try

--- ckanext/rtpa_tet_dataset_combination/plugin.py
+++ ckanext/rtpa_tet_dataset_combination/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class Rtpa_Tet_Dataset_CombinationPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IResourceView, inherit=True)
 
     # IConfigurer
 
@@ -11,3 +12,23 @@ class Rtpa_Tet_Dataset_CombinationPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'rtpa_tet_dataset_combination')
+        
+    def can_view(self, data_dict):
+        return True    
+    
+    def info(self):
+        return { 
+                 'name': 'rtpatetdatasetcombination',
+                 'title': 'Dataset Combination',
+                 'icon': 'table',
+                 'default_title': 'Dataset Combination',
+               }
+               
+               
+               
+    def view_template(self, context, data_dict):
+		return "rtpatetdatasetcombination-view.html"
+
+    def setup_template_variables(self, context, data_dict):
+		return {'dataset_resource_id':data_dict['resource']['id']}
+               
